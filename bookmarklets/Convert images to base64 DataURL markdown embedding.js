bc9d54d66adfd16d4142af675e40e552f0079d13
Marco D'Agostini <madacol10@gmail.com>
2023-07-14 20:29:21 +0200

    new: Convert images to base64 DataURL markdown embedding

---
+++ bookmarklets/Convert images to base64 DataURL markdown embedding.js
@@ -0,0 +1,34 @@
+javascript:(async function(){
+  function getBase64Image(img) {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    const dataURL = canvas.toDataURL("image/png");
+    return dataURL;
+  }
+
+  const getBase64FromUrl = async (url) => {
+    const data = await fetch(url);
+    const blob = await data.blob();
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        const base64data = reader.result;
+        resolve(base64data);
+      }
+    });
+  }
+
+  const images = document.getElementsByTagName('img');
+  [...images].forEach(async img => {
+    let base64;
+    try {
+      base64 = getBase64Image(img);
+    } catch (error) {
+      base64 = await getBase64FromUrl(img.src);
+    };
+    const markdown = `![${img.alt}](${base64})`;
+    console.log(markdown, img);
+  })
+})();
