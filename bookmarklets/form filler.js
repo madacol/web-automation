6d7344aceb92c61a3fcfbf51babc5627706b3039
Marco D'Agostini <madacol10@gmail.com>
2024-03-22 00:12:27 +0100

    new: form filler

---
+++ bookmarklets/form filler.js
@@ -0,0 +1,54 @@
+// @name Form Filler
+// @description This script records the values of all form fields and generates a bookmarklet that will restore them. To use it, fill out the form fields and click this script, a link with the bookmarklet will show up, save it and whenever you want to restore the form fields with the same values, click the bookmarklet.
+
+(() => {
+    const scriptCode = ['(() => {'];
+    [...document.forms].forEach((form, formIndex) => {
+        [...form.elements].forEach((element, elementIndex) => {
+            if (!element?.name) return; // Skip elements without a name
+
+            const elementType = element.type.toLowerCase();
+            const elementSelector = `document.forms[${formIndex}].elements[${elementIndex}]`;
+
+            switch (elementType) {
+                case 'text':
+                case 'password':
+                case 'textarea':
+                case 'email':
+                case 'date':
+                case 'number':
+                case 'select-one':
+                    const value = JSON.stringify(element.value);
+                    scriptCode.push(`${elementSelector}.value = ${value};`);
+                    break;
+                case 'checkbox':
+                case 'radio':
+                    scriptCode.push(`${elementSelector}.checked = ${element.checked};`);
+                    break;
+                case 'select-multiple':
+                    [...element.options].forEach((opt, idx) => {
+                        scriptCode.push(`${elementSelector}.options[${idx}].selected = ${opt.selected};`);
+                    });
+                    break;
+                // Include additional input types as needed
+            }
+        });
+    });
+
+    scriptCode.push('})();');
+    const bookmarkletCode = `javascript:${encodeURIComponent(scriptCode.join('\n'))}`;
+    
+    // Create the bookmarklet link and append it to the body of the page
+    const link = document.createElement('a');
+    link.href = bookmarkletCode;
+    link.textContent = 'Fill Form Bookmarklet'; // Text to display on the link
+    link.style.position = 'fixed';
+    link.style.top = '10px';
+    link.style.left = '10px';
+    link.style.backgroundColor = '#f0f0f0';
+    link.style.padding = '5px';
+    link.style.border = '1px solid #ccc';
+    link.style.borderRadius = '3px';
+    link.style.zIndex = '10000';
+    document.body.appendChild(link);
+})();
