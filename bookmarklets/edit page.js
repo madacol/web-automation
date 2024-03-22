f933e1e02c7d2b23d77432273480e709b9701367
Marco D'Agostini <madacol10@gmail.com>
2024-03-22 08:54:40 +0100

    new: Edit Page

---
+++ bookmarklets/edit page.js
@@ -0,0 +1,5 @@
+// @name Edit Page
+// @description This script makes the entire page editable, by adding the contentEditable attribute to the body element.
+
+document.body.contentEditable = true;
+document.body.spellcheck = false;
