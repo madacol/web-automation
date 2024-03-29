7aacee7bb6c566e6110682bb3097303c433e5db7
Marco D'Agostini <madacol10@gmail.com>
2024-03-29 15:52:56 +0100

    validate that an input or textarea is selected, and improved code

--- bookmarklets/Underline Text Using Unicode.js
+++ bookmarklets/Underline Text Using Unicode.js
@@ -1,24 +1,25 @@
 // @name Underline Text Using Unicode
-// @description Underlines the selected text on inputs and textareas using Unicode combining characters. Each character in the selection is combined with the Unicode character U+0332 (COMBINING LOW LINE) to simulate underlining.
+// @description Underlines the selected text in inputs or textareas using Unicode combining characters. Each character in the selection is combined with the Unicode character U+0332 (COMBINING LOW LINE) to simulate underlining.
 
 (function() {
-    // Get the currently active element on the page. This is usually the input field or textarea where the user has made a selection.
-    var el = document.activeElement;
-    
-    // Retrieve the start and end positions of the selection within the input field or textarea.
-    var start = el.selectionStart;
-    var end = el.selectionEnd;
+    /** @type {HTMLInputElement | HTMLTextAreaElement} */
+    const input = document.activeElement;
 
-    // Retrieve the current value of the input field or textarea.
-    var val = el.value;
+    // Check if the active element is an input or textarea
+    if (!['INPUT', 'TEXTAREA'].includes(input.tagName)) {
+        return alert('Please select some text in an input or textarea.');
+    }
+
+    const { value, selectionStart, selectionEnd } = input;
 
     // Extract the selected text, and replace each non-space character with itself followed by the Unicode combining low line.
-    var text = val.substring(start, end).replace(/\S/g, "$&\u0332");
+    const selectedText = value.substring(selectionStart, selectionEnd);
+    const newText = selectedText.replace(/\S/g, "$&\u0332");
 
     // Reconstruct the value with the underlined selection inserted back into its original position.
-    el.value = val.substring(0, start) + text + val.substring(end);
+    input.value = value.split(selectedText).join(newText);
 
     // Update the selection to highlight the newly underlined text.
-    el.selectionStart = start;
-    el.selectionEnd = start + text.length;
+    input.selectionStart = selectionStart;
+    input.selectionEnd = selectionStart + newText.length;
 })();
