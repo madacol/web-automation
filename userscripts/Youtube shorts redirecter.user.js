752e2ae4b6f8e8f13582a8a8fe1ec577d843a1dd
Marco D'Agostini <madacol10@gmail.com>
2024-04-05 16:01:36 +0200

    simplify script and remove search params

--- userscripts/Youtube shorts redirecter.user.js
+++ userscripts/Youtube shorts redirecter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Shorts Redirecter
-// @version      0.2
+// @version      0.3
 // @description  Redirects youtube yhorts to the traditional video player
 // @author       madacol
 // @match        *://*.youtube.com/shorts/*
@@ -8,6 +8,4 @@
 // @grant        none
 // ==/UserScript==
 
-const url = new URL(window.location.href);
-const videoId = url.pathname.split('/').pop();
-window.location.href = `https://www.youtube.com/watch?v=${videoId}`;
+window.location.href = window.location.href.replace(/\?.*/, '').replace('/shorts/', '/watch?v=');
