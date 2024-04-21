0a9af05f785c4092cae1e71b6fd6d389bb66a51d
Marco D'Agostini <madacol10@gmail.com>
2024-04-21 10:54:14 +0200

    add comments

--- bookmarklets/No Hijack.js
+++ bookmarklets/No Hijack.js
@@ -2,6 +2,7 @@
 // @description Blocks common event hijacking techniques, like disabling paste, right-click, text selection, and some keyboard shortcuts. Run the bookmarklet again to undo.
 
 javascript: (() => {
+    /* Undo the block */
     if (window._NoHijackHandlers) {
         for (const [eventName, handler] of Object.entries(window._NoHijackHandlers)) {
             document.removeEventListener(eventName, handler, true);
@@ -10,7 +11,7 @@ javascript: (() => {
         return;
     }
 
-
+    /* Stop events from propagating */
     const block = e => e.stopImmediatePropagation();
     window._NoHijackHandlers = {
         copy: block,
