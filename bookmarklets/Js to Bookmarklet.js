877963df8c29096f6ce7ee0c8eb9164ce49496b3
Marco D'Agostini <madacol10@gmail.com>
2024-06-05 12:44:33 +0200

    simplified modal

--- bookmarklets/Js to Bookmarklet.js
+++ bookmarklets/Js to Bookmarklet.js
@@ -13,34 +13,16 @@
     const a = document.createElement('a');
     a.href = `javascript:(()=>{${encodeURIComponent(selection)}})();`;
     a.textContent = 'Drag to bookmarks bar to save.';
-    a.style.display = 'block';
 
     // Show link in a modal
     showModal(a);
 
     function showModal(HtmlContent) {
-        const modal = document.createElement('div');
-        const modalContent = document.createElement('div');
-        // close modal when clicking outside of it
-        modal.onclick = e => { if (e.target === modal) modal.remove(); };
-
-        modalContent.appendChild(HtmlContent);
-        modal.appendChild(modalContent);
+        const modal = document.createElement('dialog');
+        modal.appendChild(HtmlContent);
         document.body.appendChild(modal);
-
-        // Style modal
-        modal.style.position = 'fixed';
-        modal.style.zIndex = 10000;
-        modal.style.left = '0';
-        modal.style.top = '0';
-        modal.style.width = '100%';
-        modal.style.height = '100%';
-        modal.style.backgroundColor = 'rgba(0,0,0,0.5)';
-        modal.style.display = 'flex';
-        modal.style.justifyContent = 'center';
-        modal.style.alignItems = 'center';
-        modalContent.style.backgroundColor = '#fff';
-        modalContent.style.padding = '20px';
-        modalContent.style.borderRadius = '5px';
+        modal.showModal();
+        // close modal when clicking outside its content
+        modal.onclick = e => { if (e.target === modal) modal.remove(); };
     }
 })();
